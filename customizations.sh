9fefaa31420902c3e86b04eef91ff51f6df67242
Espedito Ferreira Alves <espeditoalves123@hotmail.com>
2025-01-12 23:31:29 -0300

    Adiciona atualização no arquivo README sobre a nova forma de criar o ambiente com o docker

--- customizations.sh
+++ customizations.sh
@@ -6,5 +6,8 @@ curl -sSL https://install.python-poetry.org | python3 - --version 1.8.4
 # Baixar o driver JDBC do PostgreSQL
 wget https://jdbc.postgresql.org/download/postgresql-42.2.20.jar -P /usr/local/spark/jars/
 
+# Use esse código para saber se foi intalado corretamente
+# ls /usr/local/spark/jars/
+
 # Outras configurações adicionais
 echo "Customizações aplicadas com sucesso!"
